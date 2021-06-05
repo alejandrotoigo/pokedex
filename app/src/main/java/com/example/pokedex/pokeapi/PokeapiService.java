05c08d0a3b766350c2fc73b8c53ac49b40a6e06d
Alejandro Toigo <alejandro.toigo@jcor.com.ar>
2021-06-05 00:23:42 -0300

    Recycler view con scroll reload

---
+++ app/src/main/java/com/example/pokedex/pokeapi/PokeapiService.java
@@ -0,0 +1,13 @@
+package com.example.pokedex.pokeapi;
+
+import com.example.pokedex.Models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeapiService {
+
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit") int limit, @Query("offset") int offset);
+}
